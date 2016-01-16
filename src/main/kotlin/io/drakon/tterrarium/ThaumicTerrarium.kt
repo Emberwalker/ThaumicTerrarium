9e288b67abf066d373f13692fd41ffff49b73f55
Arkan Emberwalker <arkan@drakon.io>
2016-01-16 20:03:20 +0000

    Skeleton stuff

---
+++ src/main/kotlin/io/drakon/tterrarium/ThaumicTerrarium.kt
@@ -0,0 +1,38 @@
+package io.drakon.tterrarium
+
+import io.drakon.tterrarium.lib.Const
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.common.Mod.EventHandler
+import net.minecraftforge.fml.common.event.FMLInitializationEvent
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent
+import org.apache.logging.log4j.LogManager
+
+/**
+ * Flux and taint meets environmentalism.
+ *
+ * @author Arkan <arkan@drakon.io>
+ */
+@Mod(modid = Const.MOD_ID, name = Const.MOD_NAME, version = Const.VERSION,
+        dependencies = "required-after:Thaumcraft@[5.1.0.0,)",
+        modLanguageAdapter = "io.drakon.forgelin.KotlinAdapter")
+object ThaumicTerrarium {
+
+    private val logger = LogManager.getLogger()
+
+    @EventHandler
+    fun preinit(evt:FMLPreInitializationEvent) {
+        logger.info("Preinit.")
+    }
+
+    @EventHandler
+    fun init(evt:FMLInitializationEvent) {
+        logger.info("Init.")
+    }
+
+    @EventHandler
+    fun postinit(evt:FMLPostInitializationEvent) {
+        logger.info("Postinit.")
+    }
+
+}
---
+++ src/main/kotlin/io/drakon/tterrarium/lib/Const.kt
@@ -0,0 +1,14 @@
+package io.drakon.tterrarium.lib
+
+/**
+ * General mod constants
+ *
+ * @author Arkan <arkan@drakon.io>
+ */
+object Const {
+
+    public const val MOD_ID = "tterrarium"
+    public const val MOD_NAME = "Thaumic Terrarium"
+    public const val VERSION = "@VERSION@"
+
+}
